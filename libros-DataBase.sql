9a4e354f4bcbe1350a4f474ae774222902b21158
santiagocarrilloT <santiago.carrillo@correounivalle.edu.co>
2025-03-18 12:30:18 -0500

    actualizacionDB

--- libros-DataBase.sql
+++ libros-DataBase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-03-2025 a las 16:00:18
+-- Tiempo de generación: 18-03-2025 a las 18:26:45
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -40,11 +40,11 @@ CREATE TABLE `libros` (
 --
 
 INSERT INTO `libros` (`id_libro`, `titulo`, `autor`, `genero`, `stock`) VALUES
-(1, 'El nombre de la rosa', 'Umberto Eco', 'Novela', 1),
-(3, 'Guerra y paz', 'León Tolstói', 'Historia', 1),
-(4, 'Los viajes de Gulliver', 'Jonathan Swift', 'Historia', 3),
-(7, 'Aprender a programar con C#', 'Héctor De León Guevara', 'Programación', 5),
-(20, 'Titulo Test', 'Autor Test', '', 0);
+(21, 'El nombre de la rosa', 'Umberto Eco', 'Novela', 1),
+(22, 'Guerra y paz', 'León Tolstói', 'Historia', 1),
+(23, 'Los viajes de Gulliver', 'Jonathan Swift', 'Historia', 3),
+(24, 'Aprender a programar con C#', 'Héctor De León Guevara', 'Programación', 5),
+(30, 'Titulo Test', 'Autor Test', '', 0);
 
 --
 -- Índices para tablas volcadas
@@ -64,7 +64,7 @@ ALTER TABLE `libros`
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_libro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
